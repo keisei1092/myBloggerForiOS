d138bab3e7c1323222f8d8f818be7ace1416e4e4
keisei1092 <keisei_1092@yahoo.co.jp>
2015-06-23 14:26:30 +0900

    Initial Commit

---
+++ myBloggerForiOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myBloggerForiOS
+//
+//  Created by Saito Keisei on 6/23/15.
+//  Copyright (c) 2015 keisei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ myBloggerForiOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  myBloggerForiOS
+//
+//  Created by Saito Keisei on 6/23/15.
+//  Copyright (c) 2015 keisei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
